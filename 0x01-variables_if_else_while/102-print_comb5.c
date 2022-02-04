59955235fa71771c3f32d3d6a131836316666e7b
KevinFRX <xerokf13@gmail.com>
2022-02-04 08:27:22 -0800

    1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+
+	while (a <= 99)
+	{
+		while (b <= 99)
+		{
+			if (a != b && a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (!(a == 98 && b == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	putchar('\n');
+	return (0);
+
+}
