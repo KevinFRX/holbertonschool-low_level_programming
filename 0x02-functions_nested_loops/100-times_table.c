04e1beab33f77a15a9c72c53338546eb5c721932
KevinFRX <xerokf13@gmail.com>
2022-02-07 11:27:18 -0800

    2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,11 +25,19 @@ void print_times_table(int n)
 					if (k < 10)
 					{
 						_putchar(' ');
+						_putchar(' ');
 						_putchar(k + '0');
+					}
+					else if (k < 100)
+					{
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
 					}
 					else
 					{
-						_putchar((k / 10) + '0');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 100) % 10) + '0');
 						_putchar((k % 10) + '0');
 					}
 				}
