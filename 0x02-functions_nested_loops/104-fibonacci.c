780c1aae223b9338b13080cdc90734cd9a380031
KevinFRX <xerokf13@gmail.com>
2022-02-07 15:10:17 -0800

    1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	unsigned long i, a = 0, b = 1, c;
 
-	for (i = 0; i < 1; i++)
+	for (i = 0; i < 98; i++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		if (i != 0)
+		if (i != 97)
 		{
 			printf("%lu, ", c);
 		}
