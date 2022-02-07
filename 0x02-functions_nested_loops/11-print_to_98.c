7cf51e6ff7c143acf77f6eea9e18fee3dd13118a
KevinFRX <xerokf13@gmail.com>
2022-02-07 10:49:03 -0800

    1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,40 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		_putchar(i + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
 	_putchar('\n');
 }
