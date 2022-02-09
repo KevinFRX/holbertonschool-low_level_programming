0be0a35033055e98332363a1bd4457e5d68e5ed7
KevinFRX <xerokf13@gmail.com>
2022-02-09 09:44:28 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	long i, num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == o)
+
+	}
+}
