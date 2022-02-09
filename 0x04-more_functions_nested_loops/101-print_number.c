b6afe937fe7280521624677cf68f24fcfddd4487
KevinFRX <xerokf13@gmail.com>
2022-02-09 11:19:09 -0800

    3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,24 +11,41 @@ void print_number(int n)
 {
 	int a, b, c;
 
-	if (n < 0)
+	if (n == -2147483648)
 	{
-		a = n * -1;
-		_putchar(45);
+		_putchar('-');
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
 	}
 	else
 	{
-		a = n;
-	}
-	b = a;
-	c = 1;
-	while (b >= 10)
-	{
-		b /= 10;
-		c *= 10;
-	}
-	for (; c >= 1; c /= 10)
-	{
-		_putchar(((a / c) % 10) + 48);
+		if (n < 0)
+		{
+			a = n * -1;
+			_putchar(45);
+		}
+		else
+		{
+			a = n;
+		}
+		b = a;
+		c = 1;
+		while (b >= 10)
+		{
+			b /= 10;
+			c *= 10;
+		}
+		for (; c >= 1; c /= 10)
+		{
+			_putchar(((a / c) % 10) + 48);
+		}
 	}
 }
