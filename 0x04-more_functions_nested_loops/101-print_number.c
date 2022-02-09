45c2b70165e9379fdc4d03174fee22381fe15445
KevinFRX <xerokf13@gmail.com>
2022-02-09 10:52:48 -0800

    1

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: number
+*
+* Return: Nothing.
+*/
+
+void print_number(int n)
+{
+	int a, b, c;
+
+	if (n < 0)
+	{
+		a = n * -1;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+	}
+	b = a;
+	c = 1;
+	while (b >= 10)
+	{
+		b /= 10;
+		c *= 10;
+	}
+	for (; c >= 1; c /= 10)
+	{
+		_putchar(((a / c) % 10) + '0');
+	}
+}
