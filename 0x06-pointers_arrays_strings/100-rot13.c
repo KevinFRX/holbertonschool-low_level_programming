d150a474a2ec575a7f375295804101eba0d832f1
KevinFRX <xerokf13@gmail.com>
2022-02-16 04:55:19 -0800

    1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot13
+* @s: the string
+*
+* Return: The new string
+*/
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			{
+				s[i] += 13;
+			}
+			else
+			{
+				s[i] -= 13;
+			}
+		}
+	}
+	return (s);
+}
