af383bd99892ae8703baffc2543fa44e09a4db7f
KevinFRX <xerokf13@gmail.com>
2022-02-23 09:11:00 -0800

    1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,14 +9,14 @@
 
 int _sqrt_recursion(int n)
 {
-        if (n >= 0 )
-        {
-                return (root(n, 1));
-        }
-        else
-        {
-                return (-1);
-        }
+	if (n >= 0)
+	{
+		return (root(n, 1));
+	}
+	else
+	{
+		return (-1);
+	}
 }
 
 /**
