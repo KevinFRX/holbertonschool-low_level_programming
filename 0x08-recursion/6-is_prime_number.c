e6c1c1bff8d5bac11547fec00a96d3d2106d14d8
KevinFRX <xerokf13@gmail.com>
2022-02-23 09:39:58 -0800

    1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,6 +9,10 @@
 
 int is_prime_number(int n)
 {
+	if (n <= 0)
+	{
+		return (0);
+	}
 	return (prime(n, 1));
 }
 
@@ -22,10 +26,6 @@ int is_prime_number(int n)
 
 int prime(int n, int a)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
 	if (n % a == 0 && a > 1)
 	{
 		return (0);
