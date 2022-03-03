a6264f592fad3a256886e1e4e35d5ef76b3944e4
KevinFRX <xerokf13@gmail.com>
2022-03-03 14:14:00 -0800

    4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,11 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < a; i++)
-		s[i] = s1[i];
+	for (i = 0; i < a; i++, j++)
+		s[j] = s1[i];
 
-	for (i = 0; i < n; i++)
-		s[a + i] = s2[i];
+	for (i = 0; i < n; i++, j++)
+		s[j] = s2[i];
 
 	s[j] = '\0';
 	return (s);
