a915ab3cf065ec7ec46be86667bfa8fe4f9c6e1d
KevinFRX <xerokf13@gmail.com>
2022-03-08 12:12:37 -0800

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOGXD
+#define DOGXD
+
+/**
+ * struct dog - dog's information
+ * @name: name
+ * @age: age
+ * @owner: owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
