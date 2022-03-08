65273362713fbeeafc35e347e91ca4e21cd9c27f
KevinFRX <xerokf13@gmail.com>
2022-03-08 14:30:13 -0800

    13

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,10 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	a = 0;
 	b = 0;
-	while (name[a++])
-		;
-	while (owner[b++])
-		;
+	while (name[a])
+		a++;
+	while (owner[b])
+		b++;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
