01640c5c670b8cd529b58ab8abf0d9e039b4ad82
KevinFRX <xerokf13@gmail.com>
2022-03-09 11:16:33 -0800

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,7 +6,7 @@
  * @size: size of the array
  * @cmp: pointer to the function to be used to compare values
  *
- * Return: the index of the first element for which the cmp function does not 
+ * Return: the index of the first element for which the cmp function does not
  * return 0, -1 if no element matches or if size <= 0
  */
 
