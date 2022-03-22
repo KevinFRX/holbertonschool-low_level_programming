08f2b9f8ff2104e45c191a9d5183e662a97a6660
KevinFRX <xerokf13@gmail.com>
2022-03-22 13:58:33 -0700

    1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,5 +10,18 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int count = 0;
 
+	if (!head)
+		return (NULL);
+
+	while (head)
+	{
+		if (count == index)
+			return (head);
+
+		head = head->next;
+		count++;
+	}
+	return (NULL);
 }
