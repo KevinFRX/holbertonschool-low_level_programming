6f39e49882ffa83f2ac8924cf6fb7ac4beba4a10
KevinFRX <xerokf13@gmail.com>
2022-03-22 18:27:32 -0700

    1

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * size_t free_listint_safe - frees a listint_t list
+ * @h: h
+ *
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_t *find_listint_loop - finds the loop in a linked list
+ * @head: head
+ *
+ * Return: address of the node where the loop starts or NULL if no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+}
