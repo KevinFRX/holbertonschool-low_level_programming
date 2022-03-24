beb18cfef91fedd9034cf609dba382e1bd56b03f
KevinFRX <xerokf13@gmail.com>
2022-03-24 13:37:28 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: n
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_binary_aux(n);
+}
+
+/**
+ * aux - aux
+ * @n: n
+ *
+ * Return: void
+ */
+
+void print_binary_aux(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	print_binary_aux(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
