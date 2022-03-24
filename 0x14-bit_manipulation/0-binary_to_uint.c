cee9db5985e52e850489ac2da6e8e6513041454d
KevinFRX <xerokf13@gmail.com>
2022-03-24 12:56:41 -0700

    2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0, m = 1;
 	unsigned int res = 0;
 
+	if (!b)
+		return (0);
+
 	while (b[i])
 		i++;
 
 	for (i--; i >= 0; i--, m *= 2)
-	{
 		if (b[i] == '1')
-		{
 			res += m;
-		}
-	}
+
 	return (res);
 }
