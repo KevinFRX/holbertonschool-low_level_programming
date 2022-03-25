02cf1e98db2e772b5b54156d653f9579baa3a72d
KevinFRX <xerokf13@gmail.com>
2022-03-24 17:03:01 -0700

    1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int x = 1;
+	char *c = (char *) &x;
+
+	if (*c == 0x10)
+		return (1);
+	else
+		return (0);
+}
