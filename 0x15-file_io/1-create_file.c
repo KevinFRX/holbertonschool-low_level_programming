f59114569562890aae470b5bb7a7628a18d857c3
KevinFRX <xerokf13@gmail.com>
2022-03-28 14:52:02 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,21 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len] != '\0')
-		len++;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content == NULL)
 		return (1);
+
+	while (text_content[len] != '\0')
+		len++;
+
 	w = write(fd, text_content, len);
 	if (w == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
 }
