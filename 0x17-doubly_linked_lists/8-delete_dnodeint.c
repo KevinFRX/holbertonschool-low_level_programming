bc26eb9bc8772b3e709f29487dec39abea0cc3f0
KevinFRX <xerokf13@gmail.com>
2022-05-05 12:49:25 -0700

    1

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - a
+ * @head: a
+ * @index: a
+ *
+ * Return: a
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *dnode;
+	unsigned int i = 1;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	temp = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		while (i < index)
+		{
+			temp = temp->next;
+			if (temp == NULL)
+			{
+				return (-1);
+			}
+			i++;
+		}
+		dnode = temp;
+		dnode = dnode->next;
+		temp->next = dnode->next;
+		free(dnode);
+	}
+	return (1);
+}
