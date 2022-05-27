c4514b500f1f3b1c3a2170790df5a3d3e14cf84d
KevinFRX <xerokf13@gmail.com>
2022-05-27 16:54:33 -0700

    2

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *aux;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 	idx = key_index((const unsigned char *)key, ht->size);
 	aux = ht->array[idx];
