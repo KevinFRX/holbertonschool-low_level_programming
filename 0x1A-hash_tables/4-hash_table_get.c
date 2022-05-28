38f7c59605251f585993179dad2108d6e73025b8
KevinFRX <xerokf13@gmail.com>
2022-05-27 17:31:10 -0700

    11

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,23 +9,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
-	const hash_node_t *aux;
+	unsigned long int idx = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
-
-	index = key_index((unsigned char *)key, ht->size);
-
-	if (ht->array[index] == NULL)
+	idx = key_index((const unsigned char *) key, ht->size);
+	if (ht->array[idx] == NULL)
 		return (NULL);
-
-	aux = ht->array[index];
-	while (aux != NULL)
+	while (ht->array[idx] != NULL)
 	{
-		if (strcmp(aux->key, key) == 0)
-			return (aux->value);
-		aux = aux->next;
+		if (strcmp(ht->array[idx]->key, key) == 0)
+			return (ht->array[idx]->value);
+		ht->array[idx] = ht->array[idx]->next;
 	}
 	return (NULL);
 }
