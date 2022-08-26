044313a62b61460be7950066db1efd2cafce7462
KevinFRX <xerokf13@gmail.com>
2022-08-26 08:23:22 -0700

    1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - x
+ * @array: x
+ * @size: x
+ * @value: x
+ *
+ * Return: the index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, m, L, R;
+
+	if (!array)
+		return (-1);
+
+	for (L = 0, R = size -1; L <= R;)
+	{
+		m = (L + R) / 2;
+
+		printf("Searching array: ");
+		for (i = L; i < R; i++)
+			printf("%i, ", array[i]);
+		printf("%i\n", array[i]);
+
+		if (array[m] == value)
+			return (m);
+
+		if (array[m] < value)
+			L = m + 1;
+
+		else
+			R = m - 1;
+	}
+	return (-1);
+}
